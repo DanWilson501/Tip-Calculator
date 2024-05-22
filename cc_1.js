2dd86b340cb53f8a711844a5d8999d094d473e81
Daniel Wilson <danwilson501@gmail.com>
2024-05-22 14:57:22 -0400

    Added an output functionality through console.log in order to display bill, tip and bill total.

--- cc_1.js
+++ cc_1.js
@@ -1,2 +1,4 @@
 const bill = 100;
 const tip = bill <= 120 && bill >= 50 ? bill * 0.1 : bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip will be ${tip}, and the total bill comes to ${bill + tip}`)
