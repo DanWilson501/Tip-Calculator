dabfcf13c572ef8113ad25c64363a0b7a75d8668
Daniel Wilson <danwilson501@gmail.com>
2024-05-22 18:37:16 -0400

    Duplicated code to showcase two data sets

--- cc_1.js
+++ cc_1.js
@@ -2,9 +2,17 @@ function calcTip(bills) {
   const tip = bills >= 50  && bills <= 300 ? bills * 0.15 : bills * 0.20;
   return tip
 }
+console.log(`Data Set 1:`)
+const bills_1 = [275,40,430];
+bills_1.forEach(bill => {
+    const tip = calcTip(bill);
+    const total = bill + tip;
+    console.log(`The bill is ${bill}, the tip is ${tip}, the total is ${total}`)
+});
 
-const bills = [275,40,430];
-bills.forEach(bill => {
+console.log(`Data Set 2:`)
+const bills_2 = [275,40,430];
+bills_2.forEach(bill => {
     const tip = calcTip(bill);
     const total = bill + tip;
     console.log(`The bill is ${bill}, the tip is ${tip}, the total is ${total}`)
