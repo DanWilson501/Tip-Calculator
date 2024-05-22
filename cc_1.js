f36059ef292bc06d245bfb7d99bbf9eddb24c65a
Daniel Wilson <danwilson501@gmail.com>
2024-05-22 14:46:00 -0400

    Initial logic for calculating tip using ternary operator

--- cc_1.js
+++ cc_1.js
@@ -1,11 +1,2 @@
-function calcTip(c,i,g,nx) {
-        const tip = c+i+g+nx;
-        console.log(`Total Tip: ${tip}`);
-  }
-
-const c = 10
-const i = 23
-const g = 120
-const nx = -12
-
-calcTip(c,i,g,nx);
+const bill = 100;
+const tip = bill <= 120 && bill >= 50 ? bill * 0.1 : bill * 0.2;
