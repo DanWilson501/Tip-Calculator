94b3c631f71d8ed76b40c121f70d0e0efccd8985
Daniel Wilson <danwilson501@gmail.com>
2024-05-22 18:43:03 -0400

    Fixed values in elements of array for bills to match data set 2 values provided

--- cc_1.js
+++ cc_1.js
@@ -2,6 +2,7 @@ function calcTip(bills) {
   const tip = bills >= 50  && bills <= 300 ? bills * 0.15 : bills * 0.20;
   return tip
 }
+
 console.log(`Data Set 1:`)
 const bills_1 = [275,40,430];
 bills_1.forEach(bill => {
@@ -11,7 +12,7 @@ bills_1.forEach(bill => {
 });
 
 console.log(`Data Set 2:`)
-const bills_2 = [275,40,430];
+const bills_2 = [125,555,44];
 bills_2.forEach(bill => {
     const tip = calcTip(bill);
     const total = bill + tip;
