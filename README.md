6ddabbf7283e6e641a5a75931e30928da90d4252
Daniel Wilson <danwilson501@gmail.com>
2024-05-21 22:33:05 -0400

    Created README.md, created html file and initialized layout for Tip Calculator script

---
+++ README.md
@@ -0,0 +1,4 @@
+# Coding-Challenge-2
+ ## Tip Calculator
+
+Write a script to create a functioning calculator to calculate the tip amount based on the bill and total cost amount.
---
+++ cc_1.js
@@ -0,0 +1,11 @@
+function CalcTip(c,i,g,nx) {
+        const tip = c+i+g+nx;
+        console.log(`Total Tip: ${tip}`);
+  }
+
+const c = 10
+const i = 23
+const g = 120
+const nx = -12
+
+tipCalc(c,i,g,nx);
