4ecaef39f5eb0a2398cea8ceee3c9c63a5cce44c
Daniel Wilson <danwilson501@gmail.com>
2024-05-22 17:32:29 -0400

    Implement array for bills from data set 1. Need to correct total to display calculation and not combination of values

--- cc_1.js
+++ cc_1.js
@@ -1,9 +1,9 @@
-function calcTip(bill) {
-  const tip = bill >= 50  && bill <= 300 ? bill * 0.15 : bill * 0.20;
+function calcTip(bills) {
+  const tip = bills >= 50  && bills <= 300 ? bills * 0.15 : bills * 0.20;
   return tip
 }
 
-const bill = 100;
-const tip = calcTip(bill);
+const bills = [275,40,430]
+const tips = [calcTip(bills[0]), calcTip(bills[1]),calcTip(bills[2])]
 
-console.log(`The bill is ${bill}, the tip is ${tip}, the total is ${bill + tip}`);
+console.log(`The bill is ${bills}, the tip is ${tips}, the total is ${bills + tips}`);
