8c12cb12d6a811af9224d3df8ff5c0528d025b19
Daniel Wilson <danwilson501@gmail.com>
2024-05-22 17:06:52 -0400

    Create and test calcTip function with output display.

--- cc_1.js
+++ cc_1.js
@@ -1,4 +1,9 @@
-const bill = 100;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+function calcTip(bill) {
+  const tip = bill >= 50  && bill <= 300 ? bill * 0.15 : bill * 0.20;
+  return tip
+}
 
-console.log(`The bill is ${bill}, the tip is ${tip}, the total is ${bill+tip}`);
+const bill = 100;
+const tip = calcTip(bill);
+
+console.log(`The bill is ${bill}, the tip is ${tip}, the total is ${bill + tip}`);
