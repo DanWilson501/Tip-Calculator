f243021c53e5741bf7d571c768a8fafc94d3867d
Daniel Wilson <danwilson501@gmail.com>
2024-05-22 21:15:54 -0400

    Cleaned up duplicate code while retaining descriptive console output to display both data sets and calculations. Added U number.

--- cc_1.js
+++ cc_1.js
@@ -1,20 +1,20 @@
+// U5223-1368
+
 function calcTip(bills) {
   const tip = bills >= 50  && bills <= 300 ? bills * 0.15 : bills * 0.20;
   return tip
 }
 
+function getTotals(bills) {
+  bills.forEach(bill => {
+      const tip = calcTip(bill);
+      const total = bill + tip;
+      console.log(`The bill is ${bill}, the tip is ${tip}, the total is ${total}`)
+  });
+}
+
 console.log(`Data Set 1:`)
-const bills_1 = [275,40,430];
-bills_1.forEach(bill => {
-    const tip = calcTip(bill);
-    const total = bill + tip;
-    console.log(`The bill is ${bill}, the tip is ${tip}, the total is ${total}`)
-});
+getTotals([275,40,430]);
 
 console.log(`Data Set 2:`)
-const bills_2 = [125,555,44];
-bills_2.forEach(bill => {
-    const tip = calcTip(bill);
-    const total = bill + tip;
-    console.log(`The bill is ${bill}, the tip is ${tip}, the total is ${total}`)
-});
+getTotals([125,555,44]);
